e7ad1d9cb0e3f7c75383eb623fdaa03d358bd201
delgps <delgps@yahoo.com.br>
2024-01-22 11:04:16 -0300

    calculando velocidade do piloto mais proximo e velocidade media

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
 require "./api/consulta.php";
 $pilots = [];
+$pilots_time = [];
+$v_media = [];
 $position = 1;
-$temp_start = '';
-$temp_finish = '';
-$duration = '';
 
 uasort($results, function($a,$b)
 {   //Ordenando o array por hora do menor para o maior
@@ -14,26 +13,7 @@ uasort($results, function($a,$b)
     return ($a['Hora'] < $b['Hora']) ? -1 : 1;
 });
 
-$temp_start = $results[0]['Hora'];
-$temp_finish = $results[count($results)-1]['Hora'];
-$inicio = DateTime::createFromFormat("H:i:s.u", $temp_start);
-$fim = DateTime::createFromFormat("H:i:s.u", $temp_finish);
-
-// Calcular a diferença de tempo
-$diferenca = $inicio->diff($fim);
-
-// Obter a diferença em horas minutos segundos e milisegundos
-if ($diferenca->h != 0)
-    $duration = $diferenca->h.' horas ';
-
-if ($diferenca->i != 0)
-    $duration .= $diferenca->i.' minutos ';
-
-if ($diferenca->s != 0)
-    $duration .= $diferenca->s.' segundos ';
-
-if ($diferenca->f != 0)
-    $duration .= str_replace(".","",$diferenca->f).' milisegundos';
+$timeRun = calcHours($results[0]['Hora'], $results[count($results)-1]['Hora']);
 
 uasort($results, function($a,$b)
 {   //Ordenando o array por numero de voltas do maior para o menor
@@ -43,28 +23,57 @@ uasort($results, function($a,$b)
     return ($a['NVolta'] > $b['NVolta']) ? -1 : 1;
 });
 
-
 foreach($results as $result)
 {   //Percorre o array eliminando os duplicados
 
-    if(empty($temp_start))
-        $temp_start = $result['Hora'];
-
     if(empty($pilots[$result['Id']]))
     {
-        $result['position'] = $position;
+        $result['Position'] = $position;
         $pilots[$result['Id']] = $result;
         $position++;
 
         if(count($pilots) > 1)
-        {
+        {   //Calcula o tempo para o piloto da frente 
             $before_pilot = $pilots[$last_key];
+            $duration     = calcHours($result['Hora'], $before_pilot['Hora']);
+            $pilots_time[] = [
+                "Pilot_Duration" => $duration,
+                "Pilot_Before"   => $before_pilot['Piloto'],
+                "Pilot_After"    => $result['Piloto']
+            ];
         }
     }
     $last_key = $result['Id'];
-    $temp_finish = $result['Hora'];
+    $v_media[$result['Id']] = [
+        'VMedia'   => ($v_media[$result['Id']]['VMedia']??0) + str_replace(',','.',$result['VMedia']),
+        'QtdVolta' => ($v_media[$result['Id']]['QtdVolta']??0) + 1,
+        'Piloto'   => $result['Piloto']
+    ];
 }
 
+function calcHours($temp_start, $temp_finish)
+{
+    $inicio   = DateTime::createFromFormat("H:i:s.u", $temp_start);
+    $fim      = DateTime::createFromFormat("H:i:s.u", $temp_finish);
+    $duration = '';
+    // Calcular a diferença de tempo
+    $diferenca = $inicio->diff($fim);
+
+    // Obter a diferença em horas minutos segundos e milisegundos
+    if ($diferenca->h != 0)
+        $duration = $diferenca->h.' horas ';
+
+    if ($diferenca->i != 0)
+        $duration .= $diferenca->i.' minutos ';
+
+    if ($diferenca->s != 0)
+        $duration .= $diferenca->s.' segundos ';
+
+    if ($diferenca->f != 0)
+        $duration .= str_replace(".","",$diferenca->f).' milisegundos';
+    
+    return $duration;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -86,14 +95,35 @@ foreach($results as $result)
         <div class="body">
             <?php
             foreach($pilots as $pilot){
-                echo "<div>".$pilot['position']."</div>";
+                echo "<div>".$pilot['Position']."</div>";
                 echo "<div>".$pilot['Id']."</div>";
                 echo "<div>".$pilot['Piloto']."</div>";
                 echo "<div>".$pilot['NVolta']."</div>";
             }
             ?>
         </div>
-        <label><p align=left><strong>Duração da Corrida: <?=$duration?></strong></p></label>
+        <label><p align=left><strong>Duração da Corrida: <?=$timeRun?></strong></p></label>
+        <div>
+            <?php
+            foreach($pilots_time as $pilots){
+                echo "<div>".$pilots['Pilot_After']." chegou ".$pilots['Pilot_Duration']." após ".$pilots['Pilot_Before']."</div>";
+            }
+            ?>
+        </div>
+
+        <label><p align=left><strong>Velocidade Média de Cada Corredor Durante Toda Corrida</strong></p></label>
+        <div>
+            <table>
+                <?php
+                foreach($v_media as $media){
+                    echo "<tr>";
+                    echo "<td>".$media['Piloto']."</td>";
+                    echo "<td>".number_format(($media['VMedia']/$media['QtdVolta']),3)."</td>";
+                    echo "</tr>";
+                }
+                ?>
+            </table>
+        </div>
     </div>
 </body>
 </html>
